ec6f109dc74af6a66fec91d8deab4cb138c6c996
NandanPrasad25 <prasadnandan2595@gmail.com>
2025-01-08 15:34:57 +0530

    Add Styles.css

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
-const heading = React.createElement("h1", {}, "Hello World from React");
+const heading = React.createElement("h1", {id: "heading"}, "Hello World from React");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
+console.log(heading)
+console.log(root)
+console.log(React)
