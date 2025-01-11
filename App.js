6190fca9753d30e187a5f7cef59b3bede806f905
NandanPrasad25 <prasadnandan2595@gmail.com>
2025-01-11 10:14:32 +0530

    Jsx super powers

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id: "heading"}, "Hello World from React");
+//React Element ( Javascript Object )
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React"
+);
+
+//React Component
+const Title = () => <h1>Namaste React</h1>;
+
+//Component composition
+const HeadingComponent = () => (
+  <>
+    <Title />
+    {heading}
+    <h3>{console.log("sdshkdhk")}</h3>
+    <h2>Best tutorial</h2>
+  </>
+);
+
+//React JSX => React.createElement(React Element/ Object) => Browser HTML Element
+//      Babel                  ReactDOM.render()
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
-console.log(heading)
-console.log(root)
-console.log(React)
+root.render(<HeadingComponent />);
+console.log(heading);
+console.log(root);
+console.log(React);
